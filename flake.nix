54a0db2a9dc8d1aa832af5d4f2560dfaa31f8b78
Leonard Pollak <leonardp@tr-host.de>
2022-05-23 07:46:34 +0200

    first commit

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "LPR flake";
+
+  inputs = {
+    #nixpkgs   = { url = "github:nixos/nixpkgs/nixos-unstable"; };
+    darknet   = { url = "github:leonardp/darknet-flake/main"; };
+  };
+
+  outputs = { self, nixpkgs, darknet, ... }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+
+      #
+      ## (un)comment the flags you want here
+      #
+
+      opencvOverride = {
+        enableGtk3 = true;
+      };
+
+      darknetOverride = {
+        #cudaSupport = true;
+        #cudnnSupport = true;
+      };
+
+    in {
+
+      devShells = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system};
+        in {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              (python3.withPackages(ps: with ps; [
+                ipython
+                jupyter
+                graphviz
+                numpy
+                pandas
+                matplotlib
+                darknet.packages.${system}.pydnet
+                (opencv4.override opencvOverride)
+              ]))
+
+              (darknet.packages.${system}.darknet.override darknetOverride
+                // { opencv = (opencv.override opencvOverride); })
+            ];
+
+            shellHook = "jupyter notebook";
+          };
+      });
+  };
+}
